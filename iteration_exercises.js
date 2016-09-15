788f2aded05485e61d35de6a3505ee6428d6f2dd
Alex & Phil <alexander.bronca@gmail.com>
2016-09-15 13:59:11 -0400

    complete iteration exercise

--- iteration_exercises.js
+++ iteration_exercises.js
@@ -4,8 +4,29 @@ Array.prototype.bubbleSort = function() {
   while (!sorted) {
     sorted = true;
 
-    for (let i = 0; i < (this.length -1); i++) {
-
+    for (let i = 0; i < (this.length - 1); i++) {
+      if (this[i] > this[i + 1]) {
+        let firstEl = this[i];
+        this[i] = this[i + 1];
+        this[i + 1] = firstEl;
+        sorted = false;
+      }
     }
   }
+  return this;
+};
+
+
+String.prototype.substrings = function() {
+  let result = [];
+
+  for (let i = 0; i < (this.length); i++) {
+    for (let j = (i + 1); j <= this.length; j++) {
+      let substring = this.slice(i, j);
+      if (result.indexOf(substring) === -1) {
+        result.push(substring);
+      }
+    }
+  }
+  return result;
 };
